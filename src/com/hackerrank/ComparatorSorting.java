c90a3c68c388f1aea12e2e878dc5601c2fdf9011
Sumit Srivastava <srivassumit@gmail.com>
2017-01-18 05:06:39 -0500

    Added Javadoc in Comparator Sorting

--- src/com/hackerrank/ComparatorSorting.java
+++ src/com/hackerrank/ComparatorSorting.java
@@ -3,6 +3,80 @@ package com.hackerrank.accepted;
 import java.util.Comparator;
 import java.util.Scanner;
 
+/**
+ * <a>https://www.hackerrank.com/challenges/ctci-comparator-sorting</a>
+ * <p>
+ * 
+ * Comparators are used to compare two objects. In this challenge, you'll create
+ * a comparator and use it to sort an array. The Player class is provided in the
+ * editor below; it has two fields:
+ * </p>
+ * <p>
+ * A string, name.<br>
+ * An integer, score.<br>
+ * </p>
+ * <p>
+ * Given an array of n Player objects, write a comparator that sorts them in
+ * order of decreasing score; if 2 or more players have the same score, sort
+ * those players alphabetically by name. To do this, you must create a Checker
+ * class that implements the Comparator interface, then write an int
+ * compare(Player a, Player b) method implementing the Comparator.compare(T o1,
+ * T o2) method.
+ * </p>
+ * <p>
+ * Input Format
+ * </p>
+ * <p>
+ * Locked stub code in the Solution class handles the following input from
+ * stdin: The first line contains an integer, n, denoting the number of players.
+ * Each of the n subsequent lines contains a player's respective name and score.
+ * </p>
+ * <p>
+ * Constraints<br>
+ * 0 <= score <= 1000<br>
+ * Two or more players can have the same name.<br>
+ * Player names consist of lowercase English alphabetic letters.
+ * </p>
+ * <p>
+ * Output Format
+ * </p>
+ * <p>
+ * You are not responsible for printing any output to stdout. Locked stub code
+ * in Solution will create a Checker object, use it to sort the Player array,
+ * and print each sorted element.
+ * </p>
+ * <p>
+ * Sample Input
+ * </p>
+ * <p>
+ * 5<br>
+ * amy 100<br>
+ * david 100<br>
+ * heraldo 50<br>
+ * aakansha 75<br>
+ * aleksa 150<br>
+ * </p>
+ * <p>
+ * Sample Output
+ * </p>
+ * <p>
+ * aleksa 150<br>
+ * amy 100<br>
+ * david 100<br>
+ * aakansha 75<br>
+ * heraldo 50<br>
+ * </p>
+ * <p>
+ * Explanation
+ * </p>
+ * <p>
+ * As you can see, the players are first sorted by decreasing score and then
+ * sorted alphabetically by name.
+ * </p>
+ * 
+ * @author Sumit
+ *
+ */
 public class ComparatorSorting {
 
 	class Checker implements Comparator<Player> {
