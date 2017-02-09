be1dec70adcdc5541cee4dc2478de9affb9091ee
Sumit Srivastava <srivassumit@gmail.com>
2017-02-09 05:14:57 -0500

    code for hourglass

---
+++ src/com/hackerrank/Hourglass.java
@@ -0,0 +1,23 @@
+package com.hackerrank.accepted;
+
+public class Hourglass {
+
+	public static void main(String[] args) {
+		int arr[][] = { { 1, 1, 1, 0, 0, 0 }, { 0, 1, 0, 0, 0, 0 }, { 1, 1, 1, 0, 0, 0 }, { 0, 0, 2, 4, 4, 0 },
+				{ 0, 0, 0, 2, 0, 0 }, { 0, 0, 1, 2, 4, 0 } };
+		int hourGlassSum = 0;
+		int maxHourGlassSum = -9999999;
+			for (int i = 0; i < 4; i++) {
+				for (int j = 0; j < 4; j++) {
+				hourGlassSum += arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j]
+						+ arr[i + 2][j + 1] + arr[i + 2][j + 2];
+				if (maxHourGlassSum < hourGlassSum) {
+					maxHourGlassSum = hourGlassSum;
+				}
+				hourGlassSum = 0;
+			}
+		}
+		System.out.println(maxHourGlassSum);
+	}
+
+}
