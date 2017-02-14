600685374ec5d38ec59737de082e1e564b3ebcba
Sumit Srivastava <srivassumit@gmail.com>
2017-02-14 02:53:03 -0500

    Code for Testing

---
+++ src/com/hackerrank/Testing.java
@@ -0,0 +1,29 @@
+package com.hackerrank.accepted;
+
+/**
+ * <a>https://www.hackerrank.com/challenges/30-testing</a>
+ * 
+ * @author Sumit
+ *
+ */
+public class Testing {
+
+	/**
+	 * # useless problem
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("5");
+		System.out.println("5 3");
+		System.out.println("-321 950 512 100 0");
+		System.out.println("4 2");
+		System.out.println("0 -132 12 154");
+		System.out.println("3 3");
+		System.out.println("-11 0 11");
+		System.out.println("6 1");
+		System.out.println("-19 0 21 -11 82 93");
+		System.out.println("7 3");
+		System.out.println("-111 0 132 243 34 46 53");
+	}
+
+}
